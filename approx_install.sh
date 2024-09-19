441285f34e9fe4cba4ed9148864509db7045dc67
gaviag-cerebras <gavia.gray@cerebras.net>
2024-09-19 16:10:43 -0400

    install script for the approximate version of the code

--- approx_install.sh
+++ approx_install.sh
@@ -15,3 +15,4 @@
 # symlink gns_utils.py and hook.py into examples/nanoGPT
 ln -s $PWD/gns_utils.py examples/nanoGPT/gns_utils.py
 ln -s $PWD/hook.py examples/nanoGPT/hook.py
+patch -p1 examples/nanoGPT/train.py approx.train.patch
